375cbee7045d27cab3bd5d8122e91aae5c59fa61
Txai-HvA <65023597+Txai-HvA@users.noreply.github.com>
2020-09-17 21:51:30 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+var body = document.querySelector("html");
+var hamburger = document.querySelector("main > div:nth-of-type(1) button:nth-of-type(1)");
+
+var bottomMenu = document.querySelector("aside");
+var chatMenu = document.querySelector("main > div:nth-of-type(2)");
+
+function toggleMenu() {
+
+    bottomMenu.classList.toggle("hideBottomMenu");
+    chatMenu.classList.toggle("showChatMenu");
+}
+
+hamburger.addEventListener("click", toggleMenu);
