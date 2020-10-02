ec1220307217bea8f71c31a3d068bbc5a95dc669
Txai-HvA <65023597+Txai-HvA@users.noreply.github.com>
2020-10-02 11:00:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,24 @@
 var body = document.querySelector("html");
 var hamburger = document.querySelector("main > div:nth-of-type(1) button:nth-of-type(1)");
 
-var bottomMenu = document.querySelector("aside");
+var bottomMenu = document.querySelector("nav div:nth-of-type(3)");
 var chatMenu = document.querySelector("main > div:nth-of-type(2)");
+
+var count;
+
+function isOdd(num) { return num % 2; }
 
 function toggleMenu() {
 
     bottomMenu.classList.toggle("hideBottomMenu");
     chatMenu.classList.toggle("showChatMenu");
+
+    // count = count + 1;
+    // if (isOdd(count)) {
+    //     body.scroll({ left: 321, behavior: "smooth" });
+    // }
+
+
 }
 
 hamburger.addEventListener("click", toggleMenu);
