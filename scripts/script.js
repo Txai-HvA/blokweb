f430459b416a48af7880dd6d6eef869f9713e0aa
Txai-HvA <65023597+Txai-HvA@users.noreply.github.com>
2020-10-09 11:23:13 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,16 @@
 
 var body = document.querySelector("html");
 var hamburger = document.querySelector("main > div:nth-of-type(1) button:nth-of-type(1)");
-var chatMenu = document.querySelector("main div:nth-of-type(2)");
+var bottomMenu = document.querySelector("nav div:nth-of-type(3)");
 
 function toggleMenu() {
     if (body.classList.contains("scrollLeft")) {
         console.log("Close chat");
-        chatMenu.classList.toggle("hideChatMenu");
+        bottomMenu.classList.toggle("showBottomMenu");
         body.classList.toggle("scrollRight");
     } else {
         console.log("Open chat");
-        chatMenu.classList.toggle("showChatMenu");
+        bottomMenu.classList.toggle("hideBottomMenu");
         body.classList.toggle("scrollLeft");
     }
 }
